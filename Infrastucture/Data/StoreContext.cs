4896372274bdb60f2a7b4a4de9e50634151c46f9
tariqaslrory <tariqaslrory@gmail.com>
2024-07-04 04:12:17 +0300

    initiat Commit

---
+++ Infrastucture/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastucture.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base
+        (options)
+        {
+        }
+        public DbSet<Product> Products { set; get; }
+
+    }
+}
